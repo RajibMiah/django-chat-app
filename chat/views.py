122b51c6bc61312a11057905edb4439167399167
rajibmiah <ahmedrajib942@gmail.com>
2022-05-28 03:04:42 +0600

    indext html created and room tempalte created

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    pass
+    return render(request , 'chat/index.html')
+
+
+def room(request, room_name):
+    context ={'room_name': room_name}
+    return render(request, 'chat/room.html', context )
