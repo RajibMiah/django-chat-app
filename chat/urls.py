f6d4a07df743cd7a80d3a722fc76e022cc079bde
rajibmiah <ahmedrajib942@gmail.com>
2022-05-28 03:53:04 +0600

    created chat room

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import home
+from .views import home ,room
+
+app_name = 'chat'
 
 urlpatterns = [
     path('', home , name= 'home' ),
-   
+    path('<str:room_name>/' , room , name= 'room'),
 ]
--- core/asgi.py
+++ core/asgi.py
@@ -1,20 +1,17 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
