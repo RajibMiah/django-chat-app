675d8fdcae26fe5bf1f2fb97ee3003f2e9c374d6
rajibmiah <ahmedrajib942@gmail.com>
2022-05-29 02:30:54 +0600

    update

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#webscoket channel layers setting
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
